81e0a3d7ad69d70de3101ae4564e05a42de89804
Sam Roehrich <sam@Sams-Mac-mini.local>
2021-04-22 13:37:33 -0700

    transfering computers

--- src/components/Dashboard/EventDashboard.tsx
+++ src/components/Dashboard/EventDashboard.tsx
@@ -1,6 +1,4 @@
 import { FC } from "react";
-import AthleteList from "./AthleteList";
-import BoulderList from "./BoulderList";
 import EventInfo from "./EventInfo";
 import { useGetEventQuery } from "../../generated/graphql";
 import Loading from "../Loading";
