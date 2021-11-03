1a2672b891efdf13c83957ff4a36ad0c17bef077
Sam Roehrich <sam_roehrich@icloud.com>
2021-11-03 10:00:31 -0700

    GM

--- src/components/AthleteInfo.tsx
+++ src/components/AthleteInfo.tsx
@@ -18,72 +18,7 @@ const AthleteOverviewTab = () => {
     <>
       <AthleteInfoOverviewPanel />
       <AthleteCalendar />
-      <AthleteNotes />
     </>
-  );
-};
-
-const AthleteNotes = () => {
-  return (
-    <div className="col-start-2 col-span-5 row-span-4 row-start-5 bg-gray-100 mb-2 rounded-md shadow-md overflow-auto">
-      <span className="p-2 text-xl">Notes</span>
-      <textarea className="w-full h-full border-none ring-0"></textarea>
-    </div>
-  );
-};
-
-const AthleteInfoRecentTraining = () => {
-  const { data, loading, error } = useGetSessionsForAthleteQuery({
-    variables: {
-      athleteId: currentAthleteId()!.toString(),
-    },
-    fetchPolicy: "cache-and-network",
-  });
-  return (
-    <div className=" col-start-2 col-span-5 row-span-4 row-start-5 bg-gray-100 mb-2 rounded-md shadow-md overflow-auto">
-      <Disclosure defaultOpen={true}>
-        {({ open }) => (
-          <>
-            <div className="">
-              <Disclosure.Button className="flex justify-between w-full h-12 items-baseline p-1">
-                <span>Training Logs</span>
-              </Disclosure.Button>
-              <Disclosure.Panel className="">
-                {data?.getCompletedSessionsForAthlete.map((item, idx) => (
-                  <div
-                    key={idx}
-                    className={classNames(
-                      idx % 2 === 0 ? "bg-white" : "",
-                      "h-14 grid grid-cols-8 p-1 grid-rows-2 gap-x-4 w-full"
-                    )}
-                  >
-                    <div className="flex flex-col col-span-3">
-                      <span className="text-xs text-gray-500">Name</span>
-                      <p className="text-sm">{item.workout.name}</p>
-                    </div>
-                    <div className="flex flex-col col-span-3">
-                      <span className="text-xs text-gray-500">Date</span>
-                      <p className="text-sm">
-                        {new Date(Date.parse(item.date)).getMonth() + 1}/
-                        {new Date(Date.parse(item.date)).getDate()}
-                      </p>
-                    </div>
-                    <div className="flex-col sm:col-span-2 md:col-span-1 md:flex md:text-left text-right">
-                      <span className="text-xs text-gray-500">RPE</span>
-                      <p className="text-sm ">{item.rpe}</p>
-                    </div>
-                    <div className="hidden md:flex flex-col col-span-2 text-right md:col-span-1">
-                      <span className="text-xs text-gray-500">Percent</span>
-                      <p className="text-sm">{item.percentCompleted}</p>
-                    </div>
-                  </div>
-                ))}
-              </Disclosure.Panel>
-            </div>
-          </>
-        )}
-      </Disclosure>
-    </div>
   );
 };
 
@@ -127,8 +62,8 @@ const AthleteInfoOverviewPanel = () => {
         <div className="hidden md:flex flex-col p-2">
           <span className="text-xs text-gray-500 text-right">Email</span>
           <div className="flex space-x-2">
-            <p className="text-lg">Parent</p>
-            <p className="text-lg">Athlete</p>
+            <p className="text-lg cursor-pointer hover:border-b-2">Parent</p>
+            <p className="text-lg cursor-pointer">Athlete</p>
           </div>
         </div>
       </div>
