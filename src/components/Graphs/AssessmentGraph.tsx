83bad6dd7e73cf1c1316caca1cf26c0543249d1b
Sam Roehrich <sam_roehrich@icloud.com>
2021-11-15 14:01:18 -0700

    converted athlete graph to use chartjs

---
+++ src/components/Graphs/AssessmentGraph.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const AssessmentGraph: FC = () => {
+  return (
+    <div>
+      <div id="assessment-graph"></div>
+    </div>
+  );
+};
+
+export default AssessmentGraph;
